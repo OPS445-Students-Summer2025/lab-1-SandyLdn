a80a793efa4f7b1ddba49407f61818fc1a97c98c
Sandra Foster <tsfoster@myseneca.ca>
2025-05-16 17:26:28 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+name = 'Isaac'
+age = 72
+
+print(name + ' is ' + str(age) + ' years old!')
+
+#num1 = 5
+#num2 = 10
+
+#print(num1)
+#print(num2)
+
+#sum = num1 + num2
+
+#print(sum)
+
+#print('The sum is: ' + str(sum))
